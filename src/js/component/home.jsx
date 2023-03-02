90e2b1abbd946609c0ffb61220c35354f2251cd9
meryvl <meryvalerolaguna@gmail.com>
2023-03-02 18:23:19 +0000

    my commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -36,17 +36,19 @@ const changeLigth = () => {
       setSelectedColor("purple");
     }else{setSelectedColor('red')}
   };
-
+  const clickLight= (e) => {
+    setSelectedColor(e.target.id);
+  };
 
 	return (
 		<>
 		
 		<div>	
 		<div className="container bg-dark semaforo">
-		<div className={"light red"+(selectedColor === "red" ? " glow " : " " )}></div>
-		<div className={"light yellow"+(selectedColor === "yellow" ? " glow " : " " )}></div>
-		<div className={"light green"+(selectedColor === "green" ? " glow " : " " )}></div>
-		<div className={purple}></div>
+		<div className={"light red"+(selectedColor === "red" ? " glow " : " " )} id="red" onClick={clickLight}></div>
+		<div className={"light yellow"+(selectedColor === "yellow" ? " glow " : " " )} id="yellow" onClick={clickLight}></div>
+		<div className={"light green"+(selectedColor === "green" ? " glow " : " " )} id="green" onClick={clickLight}></div>
+		<div className={purple} id="purple" onClick={clickLight}></div>
 		</div>
 		<div className=" d-flex justify-content-center m-3">
 		<button type="button" class="btn btn-success " onClick={start}>Start ligth</button>
