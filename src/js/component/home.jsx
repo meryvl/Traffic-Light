c3242b344a946a5eeef3bbdf05363596d34e4589
meryvl <meryvalerolaguna@gmail.com>
2023-03-02 21:51:27 +0000

    my commit new

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -42,20 +42,21 @@ const changeLigth = () => {
 
 	return (
 		<>
-		
-		<div>	
-		<div className="container bg-dark semaforo">
-		<div className={"light red"+(selectedColor === "red" ? " glow " : " " )} id="red" onClick={clickLight}></div>
-		<div className={"light yellow"+(selectedColor === "yellow" ? " glow " : " " )} id="yellow" onClick={clickLight}></div>
-		<div className={"light green"+(selectedColor === "green" ? " glow " : " " )} id="green" onClick={clickLight}></div>
-		<div className={purple} id="purple" onClick={clickLight}></div>
+		<div className="container bg-dark semaforo">	
+			<div className={"light red"+(selectedColor === "red" ? " glow " : " " )} id="red" onClick={clickLight}></div>
+			<div className={"light yellow"+(selectedColor === "yellow" ? " glow " : " " )} id="yellow" onClick={clickLight}></div>
+			<div className={"light green"+(selectedColor === "green" ? " glow " : " " )} id="green" onClick={clickLight}></div>
+			<div className={purple} id="purple" onClick={clickLight}></div>
 		</div>
+		
+		
+		
+		
 		<div className=" d-flex justify-content-center m-3">
 		<button type="button" class="btn btn-success " onClick={start}>Start ligth</button>
 		<button type="button" class="btn btn-secondary mx-2"onClick={changeLigth}>Click to change</button>
 		<button type="button" class="btn btn-dark " onClick={createLigth} >Add light</button>
 	
-		</div>
 		</div>
 		</>
 	);
