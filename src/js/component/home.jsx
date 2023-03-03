ac1c843713239721994b2704c7b6e9b785ce0226
meryvl <meryvalerolaguna@gmail.com>
2023-03-03 11:04:55 +0000

    new commit purple

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,30 +3,41 @@ import React, { useEffect, useState } from "react";
 //include images into your bundle
 //create your first component
 const Home = () => {
-const color=['red','yellow','green', 'purple']
+const color=['red','yellow','green']
 let i= 0;
-
+let interval;
 const [selectedColor , setSelectedColor] = useState("")
 let purple = selectedColor === "purple" ? "light purple glow" : "light purple ";
 const start= () => {
-	setInterval(()=>{
+	if(mostrar === true){
+		color.push('purple') 
+		
+	} //si se añade antes de darle al boton se enciende la luz morada tambien 
+	//mi intencion era parar setInterval cuando se le dé al botton de add light o a cualquier boton pero no se me para
+interval= setInterval(()=>{
 	setSelectedColor(color[i++])
-	if(i> 3){i = 0}
-	},3000);
+	if(i> color.length -1){i = 0}
+}
+	,3000);
 	}
 
 const [mostrar, setMostrar] = useState(false);
 if (!mostrar) {
     purple += " d-none";
-  }else{
-	color.push('purple')
   }
+  if(mostrar === true){
+	color.push('purple')
+}
+
   const createLigth = () => {
+	clearInterval(interval)		
     setMostrar(!mostrar);
+	
   };
 
 
 const changeLigth = () => {
+	clearInterval(interval)			
     setSelectedColor("red");
     if (selectedColor === "red") {
       setSelectedColor("yellow");
