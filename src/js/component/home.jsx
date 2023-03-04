51fcd8e66451d48dcc42529323ab4ce33e69d86d
meryvl <meryvalerolaguna@gmail.com>
2023-03-04 03:08:19 +0000

    add styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -48,7 +48,8 @@ const changeLigth = () => {
 
 	return (
 		<>
-		<div className="container bg-dark semaforo">	
+		<div className="palo container"></div>
+		<div className="container bg-dark semaforo">
 			<div className={"light red"+(selectedColor === "red" ? " glow " : " " )} id="red" onClick={clickLight}></div>
 			<div className={"light yellow"+(selectedColor === "yellow" ? " glow " : " " )} id="yellow" onClick={clickLight}></div>
 			<div className={"light green"+(selectedColor === "green" ? " glow " : " " )} id="green" onClick={clickLight}></div>
