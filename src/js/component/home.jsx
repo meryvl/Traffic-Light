bac75cae1d2058fe6a21eca4445f575da98d3385
meryvl <meryvalerolaguna@gmail.com>
2023-03-06 21:04:32 +0000

    new componet semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,72 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import Semaforo from "./semaforo.jsx";
 
 //include images into your bundle
 //create your first component
 const Home = () => {
-const color=['red','yellow','green']
-let i= 0;
-const [selectedColor , setSelectedColor] = useState("")
-let purple = selectedColor === "purple" ? "light purple glow" : "light purple ";
+return (
+<>
+<Semaforo />
+</>
 
-const start= () => {
-	if(mostrar === true){
-		color.push('purple') 	
-	} 
-interval= setInterval(()=>{
-	setSelectedColor(color[i++])
-	if(i> color.length){i = 0}
-}
-,3000);
-}
-const [mostrar, setMostrar] = useState(false);
-if (!mostrar) {
-    purple += " d-none";
-  }
-  if(mostrar === true){
-	color.push('purple')
-}
+)
 
-  const createLigth = () => {
-    setMostrar(!mostrar);
-  };
-
-
-const changeLigth = () => {		
-    setSelectedColor("red");
-    if (selectedColor === "red") {
-      setSelectedColor("yellow");
-    } else if (selectedColor === "yellow") {
-      setSelectedColor("green");
-    } else if(selectedColor === 'green'){
-		if(mostrar === true){
-      setSelectedColor("purple");}
-    }else{setSelectedColor('red')}
-  };
-  const clickLight= (e) => {
-    setSelectedColor(e.target.id);
-  };
-
-	return (
-		<>
-		<div className="palo container"></div>
-		<div className="container bg-dark semaforo">
-			<div className={"light red"+(selectedColor === "red" ? " glow " : " " )} id="red" onClick={clickLight}></div>
-			<div className={"light yellow"+(selectedColor === "yellow" ? " glow " : " " )} id="yellow" onClick={clickLight}></div>
-			<div className={"light green"+(selectedColor === "green" ? " glow " : " " )} id="green" onClick={clickLight}></div>
-			<div className={purple} id="purple" onClick={clickLight}></div>
-		</div>
-		
-		
-		
-		
-		<div className=" d-flex justify-content-center m-3">
-		<button type="button" class="btn btn-success " onClick={start}>Start ligth</button>
-		<button type="button" class="btn btn-secondary mx-2"onClick={changeLigth}>Click to change</button>
-		<button type="button" class="btn btn-dark " onClick={createLigth} >Add light</button>
 	
-		</div>
-		</>
-	);
 };
 
 export default Home;
